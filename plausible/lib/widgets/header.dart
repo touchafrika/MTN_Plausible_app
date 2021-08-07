373b592b2e04b65b1ec57714615f47d6ede852db
touchafrika <mayikathabo@gmail.com>
2021-08-07 13:48:04 +0200

    built headers for pages

---
+++ plausible/lib/widgets/header.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+AppBar header(context, { bool isAppTitle = false, required String titleText }) {
+  return AppBar(
+    title: Text(
+      isAppTitle ? 'Plausible' : titleText,
+      style: TextStyle(
+        color: Colors.white,
+        fontFamily: isAppTitle ? 'Signatra' : "",
+        fontSize: isAppTitle ? 50.0 : 22.0,
+        ),
+    ),
+    centerTitle: true,
+    backgroundColor: Colors.purple,
+  );
+}
